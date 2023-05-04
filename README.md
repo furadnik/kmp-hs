cbff42552ef57991d8bff9fb8b1df8b560034b14
Filip <filip.uradnik9@gmail.com>
2023-05-04 16:55:20 +0200

    Fix #6 - Add documentation to both the code and README.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # kmp-hs
 Implementation of the KMP algorithm in Haskell
+
+## Kmp module
+
+The `Kmp` module exports functions for searching in text.
+
+For basic usage of the module, there is the function 
+```haskell
+Kmp.kmpFind :: Eq a => [a] -> [a] -> [Int]
+```
+This function takes a pattern ("needle") and a text ("haystack"), and returns a
+list of occurences of the needle in the haystack. Specifically, it returns a
+list of indices of the beginnings of the needle.
+
+For example:
+```haskell
+import qualified Kmp
+
+kmpFind "Hello" "HelloHello" -- returns [0, 5]
+kmpFind "abab" "ababab" -- returns [0, 2]
+kmpFind "a" "aaaa" -- returns [0..3]
+```
+
+For advanced usage, there are also the functions `Kmp.kmpFindWithStates`, which
+takes states as an argument instead of the needle. Further, the function
+`needleToStates` generates the automaton for searching the haystack and
+`kmpStep` simulates one step of that automaton.
+
+For further info about the functions, look inside `src/Kmp.hs`.
--- src/Kmp.hs
+++ src/Kmp.hs
@@ -9,26 +9,34 @@ data State a = State
   }
   deriving (Eq, Show)
 
+-- Simulate one step of the KMP algorithm.
 kmpStep states State {forward_letter = Nothing, go_backward_index = b} h = kmpStep states (states !! b) h
 kmpStep states State {forward_letter = Just f, index = i, go_backward_index = b} h
   | f == h = states !! (i + 1)
   | i == 0 = head states
   | otherwise = kmpStep states (states !! b) h
 
-emptyStates i [] = [State {index = i, go_backward_index = 0, forward_letter = Nothing}]
-emptyStates i (n : ns) = State {index = i, go_backward_index = 0, forward_letter = Just n} : emptyStates (i + 1) ns
-
+-- Check whether the current state is a final state -- it doesn't have a forward letter defined, as it is the end of the needle.
 kmpIsFound State {forward_letter = f} = isNothing f
 
+-- Build the automaton -- create a list of states without the `go_backward_index`s and then add them using `buildBackwardIndices`.
+-- We initialize the fold of buildBackwardIndices with the first two states, as those must have the `go_backward_index` pointed to the first state.
+needleToStates needle = snd $ foldl buildBackwardIndices (head states, [head states, states !! 1]) (tail (tail states))
+  where
+    states = emptyStates 0 needle
+    emptyStates i [] = [State {index = i, go_backward_index = 0, forward_letter = Nothing}]
+    emptyStates i (n : ns) = State {index = i, go_backward_index = 0, forward_letter = Just n} : emptyStates (i + 1) ns
+
+-- Add backward indices to the states.
+-- The `currentState` is the state that this state's predecessor's `go_backward_index` points to.
+-- The `states` is the list of predecessor of the state we are building.
+-- The `State` is the state we are building.
 buildBackwardIndices (currentState, states) State {index = i, forward_letter = n} = (newState, states ++ [State {index = i, forward_letter = n, go_backward_index = newIndex}])
   where
     State {forward_letter = previousLetter} = states !! (i - 1)
     newState@State {index = newIndex} = kmpStep states currentState $ fromJust previousLetter
 
-needleToStates needle = snd $ foldl buildBackwardIndices (head states, [head states, states !! 1]) (tail (tail states))
-  where
-    states = emptyStates 0 needle
-
+-- Find all occurrences of the needle in the haystack.
 kmpFind needle = kmpFindWithStates 0 states (head states)
   where
     states = needleToStates needle
